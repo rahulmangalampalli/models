993022840952f806dd6dfb17ba51305c7c5cb6ec
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2020-12-16 19:54:10 +0530

    Update generate_tfrecord.py

--- research/object_detection/generate_tfrecord.py
+++ research/object_detection/generate_tfrecord.py
@@ -34,7 +34,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'nine':
+    if row_label == 'id':
         return 1
     else:
         return 0
